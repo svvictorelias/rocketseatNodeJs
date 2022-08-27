09581e1dcd6cf7cbae68e41b7903ee5567829d94
Victor Elias <svvictorelias@gmail.com>
2022-08-26 22:28:16 -0300

    Feat: create useCase

--- rentalx/src/routes/categories.routes.ts
+++ rentalx/src/routes/categories.routes.ts
@@ -1,21 +1,16 @@
-import { response, Router } from "express";
+import { Router } from "express";
+import { createCategoryController } from "../modules/cars/useCases/createCategory";
+import { listCategoriesController } from "../modules/cars/useCases/listCategories";
 
-import { CategoriesRepository } from "../modules/cars/repositories/CategoriesRepository";
-import { CreateCategoryService } from "../modules/cars/services/CreateCategoryService";
 
 const categoriesRoutes = Router();
-const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.get("/", (request, response) => {
-  const all = categoriesRepository.list();
-  return response.json(all);
+  return listCategoriesController.handle(request, response)
 });
 
 categoriesRoutes.post("/", (request, response) => {
-  const { name, description } = request.body;
-  const createCategoryService = new CreateCategoryService(categoriesRepository);
-  createCategoryService.execute({ name, description });
-  return response.status(201).send();
+  return createCategoryController.handle(request, response)
 });
 
 export { categoriesRoutes };
